9bc2c4452fbf2ad02933135f33537ba82271feb1
baidu <baidu@MacBook-Pro.local>
2015-09-09 16:51:32 +0800

    Initial Commit

---
+++ TestDynamic/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TestDynamic
+//
+//  Created by baidu on 15/9/9.
+//  Copyright (c) 2015年 baidu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TestDynamic/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TestDynamic
+//
+//  Created by baidu on 15/9/9.
+//  Copyright (c) 2015年 baidu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
